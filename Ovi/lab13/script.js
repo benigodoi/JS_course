dd3c20e56d784043a66d46beae6c40e57fad8f7f
Ovidiu Rudi <djvidov@yahoo.com>
2022-11-06 09:33:41 +0200

    eval

--- Ovi/lab13/script.js
+++ Ovi/lab13/script.js
@@ -1,80 +1,85 @@
-var classList = ['bodyClass1','bodyClass2','bodyClass3']
-var countClass = [0 , 0 , 0]
+var classList = ['bodyClass1', 'bodyClass2', 'bodyClass3']
+var countClass = [0, 0, 0]
 
+//eu nu sunt mare fan comentariilor astea, dar daca pe tine te ajuta is ok
 /*
     functia clickMe, functia initiala de click, cu un For loop si timeout pentru apelarea urmatorei functii principale
 */
-function buttonClick(){
-
+function buttonClick() {
     for (let index = 0; index < 10; index++) {
         setTimeout(buttonClickActions, index * 1000)
-        
     }
 }
 
 /* 
     functia principala - buttonClickActions - toate actiunile se ruleaza in aceasta functie
 */
-
-function buttonClickActions(){
+function buttonClickActions() {
     debugger
-    var classIndex = generateRandomClassIndex()
-    var appliedClassIndex = classList[classIndex]
-    getBodyIDs(appliedClassIndex)
-    //countClass[classIndex]++
-    countClassIndexes(classIndex)
+    var appliedClassIndex = generateRandomClassIndex()
+    // index-ul e un numar n-are cum sa fie string
+    var appliedClass = classList[appliedClassIndex]
+    setBodyClass(appliedClass)
+    countClassIndexes(appliedClassIndex)
     displayUsedClasses()
     displayMaxResults()
 }
+
 /* 
     functia generateRandomClassFromList - va genera un id random din lista de clase
 */
-function generateRandomClassIndex(){
-
-    var newCSS = Math.floor(Math.random() * classList.length)
-    return newCSS
+function generateRandomClassIndex() {
+    var newCSSIndex = Math.floor(Math.random() * classList.length)
+    return newCSSIndex
 }
+
 /* 
     functia getBodyIDs - returneaza id'urile necesare si le stocheaza in variabile
 */
-function getBodyIDs(classIndex){
+// metodele get trebuie sa returneze ceva
+function setBodyClass(cssClass) {
+    var bodyElement = document.getElementById('body')
+    bodyElement.classList = []
+    bodyElement.classList.add(cssClass)
+}
 
-    var bodyID = document.getElementById('body')
-    bodyID.classList = []
-    bodyID.classList.add(classIndex)
-   
-} 
-   
 /* 
     functia countClassIndexes - incrementeaza array'ul countClass
 */
-function countClassIndexes(number){
-    countClass[number]++
+function countClassIndexes(appliedClassIndex) {
+    countClass[appliedClassIndex]++
 }
+
 /* 
     functia displayUsedClasses - scrie in ul numele si valoarea clasei folosite
 */
-function displayUsedClasses(){
-    var usedClassDivID = document.getElementById('claseFolosite')
+function displayUsedClasses() {
+    var claseFolositeElement = document.getElementById('claseFolosite')
     var values = ''
     for (let index = 0; index < classList.length; index++) {
         var usedClass = classList[index];
         var numOfUsed = countClass[index]
-        values += "<li> clasele utilizate sunt: " + usedClass + " si numarul lor este  " + numOfUsed + "</li>"     
+        values += "<li> clasele utilizate sunt: " + usedClass + " si numarul lor este  " + numOfUsed + "</li>"
     }
-    usedClassDivID.innerHTML=values
+    claseFolositeElement.innerHTML = values
 }
+
 /* 
     functia displayMaxResults - calculeaza clasa care a fost folosita ce-a mai des / 2 modalitati
 */
-function displayMaxResults(){
-  
-    var maxDivID = document.getElementById('maxim')
-    var maxValue=Math.max(...countClass)
-    var className = countClass.indexOf(maxValue)
+function displayMaxResults() {
+
+    var maximElement = document.getElementById('maxim')
+    var maxValue = Math.max(...countClass)
+    //asta e un int n-are cum sa fie un class name
+    var classNameIndex = countClass.indexOf(maxValue)
     //for (let index = 0; index < classList.length; index++) {
     //    var maxValue2 = countClass
     //}
-    var textResult="Cea mai folosita clasa este "  + classList[className]+ " si a fost folosita de " + maxValue+ " ori"
-    maxDivID.innerHTML=textResult
+    var textResult = "Cea mai folosita clasa este " + classList[classNameIndex] + " si a fost folosita de " + maxValue + " ori"
+    maximElement.innerHTML = textResult
 }
+
+// foarte buna implementarea. 
+// am adaugat pe ici pe colo cate un comentariu
+// astept si varianta a doua, cu for-ul 
